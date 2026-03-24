d6f794ebce86131910c90511a1f1a97a03a8fa8d
Arjay <dadivas.arjay.bscs2022@gmail.com>
2026-03-25 07:15:16 +0800

    another onel

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,7 @@ registerSW({ immediate: true })
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import { ZiggyVue } from 'ziggy-js'
 
 const appName = import.meta.env.VITE_APP_NAME || 'RJ Portfolio';
 
