7146e4995d21edede84f9ef96d9ff2b2b43830a5
Arjay <dadivas.arjay.bscs2022@gmail.com>
2026-03-25 08:36:04 +0800

    add diagnostic routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,17 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+Route::get('/ping', fn () => 'Laravel is alive');
+
+Route::get('/diag', function () {
+    return response()->json([
+        'app_env' => config('app.env'),
+        'app_debug' => config('app.debug'),
+        'has_app_key' => !empty(config('app.key')),
+        'app_url' => config('app.url'),
+    ]);
+});
+
 Route::get('/', function () {
     return Inertia::render('Welcome');
 });
